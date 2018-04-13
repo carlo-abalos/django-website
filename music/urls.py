edeaa3533f476577b9e5b6f89e6c877fdd228f51
Carlo Abalos <carloabalos24@gmail.com>
2018-04-13 08:25:04 +0800

    User Authentication and Login

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,20 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    re_path(r'^$', views.index, name='index'),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+
+    re_path(r'^register/$', views.UserFormView.as_view(), name='register'),
 
     # /music/<album_id>/
-    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/<album_id>/favorite/
-    re_path(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+    # /music/album/add/
+    re_path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    re_path(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    re_path(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete')
+
 ]
